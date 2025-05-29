7244b14a2285e032252ae7d5a01ed4c7d863faba
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2025-05-28 20:05:37 -0700

    Make changes to zmq for external connection.

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import zmq
 import json
 
+with open('reviews.json', 'r') as file:
+    reviews = json.load(file)
+
+string_reviews = json.dumps(reviews)
+
 def connect_to_review_service():
     context = zmq.Context()
     print("Client attempting to connect to server...")
-    socket = context.socket(zmq.REQ)
-    socket.connect("tcp://localhost:5555")
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://127.0.0.1:5555")
     print(f"Sending a request...")
     return socket
 
 def get_all_reviews(socket):
-    socket.send_string("get_all")
-    response = socket.recv_string()
+    socket.send_string(string_reviews)
     print("The server sent back: ")
     try:
-        return json.loads(response)
+        return json.loads(string_reviews)
     except json.JSONDecodeError:
         print("Failed to decode reviews.")
         return []
@@ -27,7 +31,7 @@ def display_reviews(reviews):
     print("\n User Reviews:")
     for i, review in enumerate(reviews, start=1):
         print(f"\nReview {i}")
-        print(f"User: {review.get('user', 'Unknown')}")
+        print(f"User: {review.get('name', 'Unknown')}")
         print(f"Rating: {'⭐' * review.get('rating', 0)} ({review.get('rating', 0)}/5)")
         print(f"Review: {review.get('review', '[No review]')}")
 
@@ -40,11 +44,13 @@ def main():
         print("1. View Reviews")
         print("2. Exit")
 
-        choice = input("Enter your choice: ")
+        choice = socket.recv_string()
+        print(f"Received choice {choice}")
 
         if choice == "1":
             try:
                 reviews = get_all_reviews(socket)
+        
                 display_reviews(reviews)
             except Exception as e:
                 print(f"Error retrieving reviews: {e}")
